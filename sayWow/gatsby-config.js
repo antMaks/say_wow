fd2a542e4a6be42027b5174090cf3ec96f35c02d
antMaks <maksimantonuk74@gmail.com>
2020-10-08 21:28:40 +0300

    code

--- sayWow/gatsby-config.js
+++ sayWow/gatsby-config.js
@@ -8,9 +8,29 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: `gatsby-plugin-typography, @pittica/gatsby-plugin-seo, gatsby-plugin-react-helmet`,
       options: {
-        pathToConfigModule: `src/utils/typography`
+        pathToConfigModule: `src/utils/typography`,
+        image: `/DEFAULT_SHARING_IMAGE.jpg`,
+        socials: {
+          instagram: {
+            username: `INSTAGRAM_USERNAME`
+          },
+          github: {
+            username: `GITHUB_USERNAME`
+          },
+          facebook: {
+            page: `FACEBOOK_PAGE_USERNAME`,
+            app: `FACEBOOK_APP_ID`
+          },
+          twitter: {
+            username: `TWITTER_USERNAME`,
+            site: `TWITTER_SITE_USERNAME`
+          },
+          linkedin: {
+            page: `LINKEDIN_PAGE_USERNAME`
+          }
+        }
       }
     }
   ]
--- sayWow/src/pages/index.js
+++ sayWow/src/pages/index.js
@@ -1,14 +1,21 @@
 import React from "react"
+import {Helmet} from "react-helmet";
 // import styled from "styled-components"
 import Header from "/Users/mac/Desktop/say_wow/say_wow/sayWow/src/Components/Header.js"
-// import PayPal from "/Users/mac/Desktop/say_wow/say_wow/sayWow/src/Components/PayPal.js"
-require("/Users/mac/Desktop/say_wow/say_wow/sayWow/src/styles/main.css")
 import Content from "/Users/mac/Desktop/say_wow/say_wow/sayWow/src/Components/Content.js"
 import Footer from "/Users/mac/Desktop/say_wow/say_wow/sayWow/src/Components/Footer.js"
+// import PayPal from "/Users/mac/Desktop/say_wow/say_wow/sayWow/src/Components/PayPal.js"
+require("/Users/mac/Desktop/say_wow/say_wow/sayWow/src/styles/main.css")
+
 
 export default function SayWow() {
   return (
     <>
+    <Helmet>
+                <meta charSet="utf-8" />
+                <title>Say wow</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+            </Helmet>
       <Header></Header>
       <Content></Content>
       <Footer></Footer>
